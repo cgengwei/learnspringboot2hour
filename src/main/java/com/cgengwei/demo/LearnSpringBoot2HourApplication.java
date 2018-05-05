e7d5d4b36137eccbd7d652f6cc30c159f02475f3
cgengwei <cgengwei@126.com>
2018-05-05 08:20:39 +0800

    V0.1.1 初始化springboot

---
+++ src/main/java/com/cgengwei/demo/LearnSpringBoot2HourApplication.java
@@ -0,0 +1,12 @@
+package com.cgengwei.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBoot2HourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBoot2HourApplication.class, args);
+	}
+}
